b7f13aaa27e49323f299904d0a798e1c4283d37a
jooeun kim <jay311311@gmail.com>
2022-05-03 23:17:16 +0900

    Initial Commit

---
+++ WeatherCloset/WeatherClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherClosetApp.swift
+//  WeatherCloset
+//
+//  Created by Jooeun Kim on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct WeatherClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
